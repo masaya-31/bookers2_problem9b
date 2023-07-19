3a11338653a50ba6a549f25396eb6a69577be1d9
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-07-19 04:45:44 +0000

    [Finish]応用課題7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,12 @@ class UsersController < ApplicationController
     @book = Book.new
     @following_users = @user.followings_users
     @follower_users = @user.followers_users
+
+    @today_book =  @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
+
   end
 
   def index
